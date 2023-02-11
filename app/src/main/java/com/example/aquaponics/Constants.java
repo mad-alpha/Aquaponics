2ca3d9b59ddf7dad8dac3e160533655dd888ae3c
Aakash Kumar <aakashkumarjagia1997@gmail.com>
2023-02-11 17:21:14 +0530

    Integrate Login API

---
+++ app/src/main/java/com/example/aquaponics/Constants.java
@@ -0,0 +1,5 @@
+package com.example.aquaponics;
+
+public class Constants {
+    public static final String HOST_URL = "http://167.71.227.120:9001";
+}
--- app/src/main/java/com/example/aquaponics/LoginActivity.java
+++ app/src/main/java/com/example/aquaponics/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.aquaponics;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,16 +11,26 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
 
+    RequestQueue requestQueue;
     Button login;
     EditText username, password;
     TextView register;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        requestQueue = Volley.newRequestQueue(LoginActivity.this);
 
         login = findViewById(R.id.login_btn_seller);
         username = findViewById(R.id.username_login_seller);
@@ -36,12 +47,31 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(username.getText().toString().equals("Kisaan1") &&
-                        password.getText().toString().equals("Kisaan1")) {
-                    startActivity(new Intent(LoginActivity.this, Homepage.class));
-                } else {
-                    Toast.makeText(LoginActivity.this, "Wrong credentials", Toast.LENGTH_SHORT).show();
-                }
+                requestQueue.add(new StringRequest(Request.Method.POST,
+                        Constants.HOST_URL + "/api/login",
+                        response -> {
+//                            Log.d("myapp_debug", response);
+                            startActivity(new Intent(LoginActivity.this, Homepage.class));
+                        },
+                        error -> {
+                            Log.e("myapp_error", error.toString());
+                            error.printStackTrace();
+                            Toast.makeText(LoginActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+                        }) {
+                        @Override
+                        protected Map<String,String> getParams(){
+                            Map<String,String> params = new HashMap<>();
+                            params.put("username", username.getText().toString());
+                            params.put("password", password.getText().toString());
+                            return params;
+                        }
+                        @Override
+                        public Map<String, String> getHeaders() {
+                            Map<String,String> params = new HashMap<>();
+                            params.put("Accept","application/json");
+                            return params;
+                        }
+                });
             }
         });
     }
